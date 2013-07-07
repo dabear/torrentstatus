c7bd77b3a653e12d06bae928c9e32b4fcc419cce
Bjorn Inge Berg <bjorninge-github-com@bjorninge.no>
2013-07-07 20:52:41 +0200

    add license and submodule

--- torrentstatus/listeners/onstart.py
+++ torrentstatus/listeners/onstart.py
@@ -6,3 +6,4 @@ def listener(args):
     send_email( "Torrent download started",
                "Download of torrent {0} started".format(args.torrentname))  
     print("Started processing file")
+    #python3 utorrentctl.py --set-props AC90C1B8E2748FA00DA922650B19B8FFCED60B2F.label="bar"
